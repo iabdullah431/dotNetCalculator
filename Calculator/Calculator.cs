2e0e19fe278ecd520ee264eab75aca2af8591be6
Abdullah Ali Aldahassi <aaldahassi@abana.com.sa>
2023-05-25 07:10:45 +0300

    Upload the project of Calculator

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+        public class CalCulator
+        {
+            // Square function
+            public int Square(int num)
+            {
+                return num * num;
+            }
+            // Add two integers and returns the sum
+            public int Add(int num1, int num2)
+            {
+                return num1 + num2;
+            }
+            // Add two integers and returns the sum
+            public double Add(double num1, double num2)
+            {
+                return num1 + num2;
+            }
+            // Multiply two integers and retuns the result
+            public int Multiply(int num1, int num2)
+            {
+                return num1 * num2;
+            }
+            // Subtracts small number from big number
+            public int Subtract(int num1, int num2)
+            {
+                if (num1 > num2)
+                {
+                    return num1 - num2;
+                }
+                return num2 - num1;
+            }
+        }
+    }
+
---
+++ Calculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalCulator sq = new CalCulator();
+            Console.WriteLine(sq.Square(8).ToString());
+            Console.WriteLine(sq.Add(8.3, 9.24).ToString());
+            Console.WriteLine(sq.Multiply(5, 8).ToString());
+            Console.WriteLine(sq.Subtract(22, 42).ToString());
+            Console.ReadLine(); 
+        }
+    }
+}
